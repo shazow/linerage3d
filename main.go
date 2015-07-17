f17e9e5f2b36a0f8a0f269af2c0f6ab2f226fe86
Andrey Petrov <andrey.petrov@shazow.net>
2015-07-17 21:26:32 +0200

    Migrate to new gomobile

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 
 	mgl "github.com/go-gl/mathgl/mgl32"
 	"golang.org/x/mobile/app"
-	"golang.org/x/mobile/event"
+	"golang.org/x/mobile/event/config"
+	"golang.org/x/mobile/event/lifecycle"
+	"golang.org/x/mobile/event/paint"
+	"golang.org/x/mobile/event/touch"
 	"golang.org/x/mobile/geom"
 	"golang.org/x/mobile/gl"
 )
@@ -91,16 +94,16 @@ func (e *Engine) Stop() {
 	gl.DeleteBuffer(e.line.VBO)
 }
 
-func (e *Engine) Config(new, old event.Config) {
+func (e *Engine) Config(new, old config.Event) {
 	e.touchLoc = geom.Point{new.Width / 2, new.Height / 2}
 	e.camera.Perspective(0.785, float32(new.Width/new.Height), 0.1, 100.0)
 }
 
-func (e *Engine) Touch(t event.Touch, c event.Config) {
+func (e *Engine) Touch(t touch.Event, c config.Event) {
 	e.touchLoc = t.Loc
 }
 
-func (e *Engine) Draw(c event.Config) {
+func (e *Engine) Draw(c config.Event) {
 	since := time.Now().Sub(e.started)
 
 	gl.ClearColor(0, 0, 0, 1)
@@ -128,7 +131,7 @@ func (e *Engine) Draw(c event.Config) {
 func main() {
 	camera := EulerCamera{}
 	shader := Shader{}
-	e := Engine{
+	engine := Engine{
 		shader: &shader,
 		camera: &camera,
 		scene: &Scene{
@@ -139,14 +142,28 @@ func main() {
 		},
 	}
 
-	app.Run(app.Callbacks{
-		Start:  e.Start,
-		Stop:   e.Stop,
-		Draw:   e.Draw,
-		Touch:  e.Touch,
-		Config: e.Config,
-	})
-
 	log.SetOutput(os.Stdout)
 
+	app.Main(func(a app.App) {
+		var c config.Event
+		for e := range a.Events() {
+			switch e := app.Filter(e).(type) {
+			case lifecycle.Event:
+				switch e.Crosses(lifecycle.StageVisible) {
+				case lifecycle.CrossOn:
+					engine.Start()
+				case lifecycle.CrossOff:
+					engine.Stop()
+				}
+			case config.Event:
+				engine.Config(e, c)
+				c = e
+			case paint.Event:
+				engine.Draw(c)
+				a.EndPaint()
+			case touch.Event:
+				engine.Touch(e, c)
+			}
+		}
+	})
 }
